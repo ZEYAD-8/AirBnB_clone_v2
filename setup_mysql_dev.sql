a44eeb70a5b14df565d0063f9683ac46dee362b1
ZEYAD-8 <zeyadosama15@gmail.com>
2024-01-31 21:27:07 +0200

    Adding a dev user w/ database

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,16 @@
+-- Prepares a MySQL server for the project
+
+-- Create or use the database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create or use the user hbnb_dev and set the password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
