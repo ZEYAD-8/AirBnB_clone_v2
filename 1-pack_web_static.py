4dbbf8b5e61249310b6f67e6ecce94b2667c1c63
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-14 01:44:24 +0200

    Deployment: Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from fabric.api import local, lcd
+from datetime import datetime
+import os
+
+def do_pack():
+    """ this function will pack the contents of the web static folder and
+    compress it into an archive named by the date and time of its creation.
+    """
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_name = f"web_static_{date}.tgz"
+        directory_name = "versions"
+        print(f"Packing web_static to {directory_name}/{archive_name}")
+
+        if not os.path.exists(directory_name):
+            os.makedirs(directory_name)
+        with lcd(directory_name):
+            local(f"tar -zcvf {archive_name} ../web_static/")
+        return f"{directory_name}/{archive_name}"
+
+    except Exception:
+        return None
