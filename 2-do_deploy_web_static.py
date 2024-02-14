e0d1a24dbe7aad7dfcdf4ab77295a852af0332bb
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-14 17:51:36 +0200

    Deployment: Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,91 +9,51 @@ import os
 env.hosts = ['34.207.156.226', '54.90.63.216']
 
 
-# def do_deploy(archive_path):
-#     """ deploys the new version to the servers listed
-#     """
-
-#     # Check if the archive exists
-#     if not os.path.exists(archive_path):
-#         return False
-
-#     try:
-#         # Transfer the archive
-#         put(archive_path, "/tmp/")
-
-#         # Extract the filename (without the extension)
-#         # file_name = archive_path.split("/")[-1].split(".")[0]
-#         file_name = os.path.basename(archive_path).split(".")[0]
-
-#         # Create the directory on the server if it doesn't exist
-#         run(f"mkdir -p /data/web_static/releases/{file_name}")
-
-#         # Decompress the archive
-#         run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-#             .format(file_name, file_name))
-
-#         # Adjust the content location
-#         up_to_releases = "/data/web_static/releases"
-#         run("mv {}/{}/web_static/* {}/{}/".format(up_to_releases,
-#                                                   file_name,
-#                                                   up_to_releases,
-#                                                   file_name))
-
-#         run(f"rm -rf /data/web_static/releases/{file_name}/web_static")
-
-#         # Remove the archive
-#         run(f"rm /tmp/{file_name}.tgz")
-
-#         # Recreate the symbolic link to the new release
-#         run(f"rm /data/web_static/current")
-#         run(f"ln -sf {up_to_releases}/{file_name} /data/web_static/current")
-
-#         # Restart nginx service to apply the changes
-#         run('sudo service nginx restart')
-
-#         # Print and return
-#         print("New version deployed!")
-#         return True
-
-#     except Exception:
-#         return False
-
-
-# Since the checker doesn't accept my way i'll try this:
-    
 def do_deploy(archive_path):
-    """Archive distributor"""
+    """ deploys the new version to the servers listed
+    """
+
+    # Check if the archive exists
+    if not os.path.exists(archive_path):
+        return False
+
     try:
-        try:
-            if os.path.exists(archive_path):
-                arc_tgz = archive_path.split("/")
-                arg_save = arc_tgz[1]
-                arc_tgz = arc_tgz[1].split('.')
-                arc_tgz = arc_tgz[0]
+        # Transfer the archive
+        put(archive_path, "/tmp/")
 
-                """Upload archive to the server"""
-                put(archive_path, '/tmp')
+        # Extract the filename (without the extension)
+        # file_name = archive_path.split("/")[-1].split(".")[0]
+        file_name = os.path.basename(archive_path).split(".")[0]
 
-                """Save folder paths in variables"""
-                uncomp_fold = '/data/web_static/releases/{}'.format(arc_tgz)
-                tmp_location = '/tmp/{}'.format(arg_save)
+        # Create the directory on the server if it doesn't exist
+        run(f"mkdir -p /data/web_static/releases/{file_name}")
 
-                """Run remote commands on the server"""
-                run('mkdir -p {}'.format(uncomp_fold))
-                run('tar -xvzf {} -C {}'.format(tmp_location, uncomp_fold))
-                run('rm {}'.format(tmp_location))
-                run('mv {}/web_static/* {}'.format(uncomp_fold, uncomp_fold))
-                run('rm -rf {}/web_static'.format(uncomp_fold))
-                run('rm -rf /data/web_static/current')
-                run('ln -sf {} /data/web_static/current'.format(uncomp_fold))
-                run('sudo service nginx restart')
-                return True
-            else:
-                print('File does not exist')
-                return False
-        except Exception as err:
-            print(err)
-            return False
+        # Decompress the archive
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(file_name, file_name))
+
+        # Adjust the content location
+        up_to_releases = "/data/web_static/releases"
+        run("mv {}/{}/web_static/* {}/{}/".format(up_to_releases,
+                                                  file_name,
+                                                  up_to_releases,
+                                                  file_name))
+
+        run(f"rm -rf /data/web_static/releases/{file_name}/web_static")
+
+        # Remove the archive
+        run(f"rm /tmp/{file_name}.tgz")
+
+        # Recreate the symbolic link to the new release
+        run(f"rm /data/web_static/current")
+        run(f"ln -sf {up_to_releases}/{file_name} /data/web_static/current")
+
+        # Restart nginx service to apply the changes
+        run('sudo service nginx restart')
+
+        # Print and return
+        print("New version deployed!")
+        return True
+
     except Exception:
-        print('Error')
         return False
