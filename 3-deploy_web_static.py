f90817cdbf4025996f9e08a3bbc2bfcaeb85aefd
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-14 07:31:51 -0800

    Full Deployment!

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,15 +7,15 @@ import os
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 env.hosts = ['34.207.156.226', '54.90.63.216']
-env.user = 'ubuntu'
 
 
 def deploy():
     """ Pack and deploy all of web static """
 
-    archive_name = do_pack()
-    if not os.path.exists(archive_name):
+    archive_path = do_pack()
+    if not archive_path:
+        print("Archive was not succesfully created")
         return False
-
-    return_value = do_deploy(archive_name)
+    
+    return_value = do_deploy(archive_path)
     return return_value
