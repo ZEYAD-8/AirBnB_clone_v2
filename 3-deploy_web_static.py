c24e28ff2c68eff8676866e1efa3cad2c9c50722
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-14 04:12:29 +0200

    Full Deployment

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Creates and distributes an archive to web servers,
+using created function deploy and pack"""
+from fabric.api import *
+import os
+
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+env.hosts = ['34.207.156.226', '54.90.63.216']
+env.user = 'ubuntu'
+
+
+def deploy():
+    """ Pack and deploy all of web static """
+
+    archive_name = do_pack()
+    if not os.path.exists(archive_name):
+        return False
+
+    return_value = do_deploy(archive_name)
+    return return_value
