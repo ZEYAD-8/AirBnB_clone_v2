37e0852a88b7df59dffa973ff04b54787900ff71
ZEYAD-8 <zeyadosama15@gmail.com>
2024-02-14 07:30:20 -0800

    Deployment: Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Fabfile that create a .tgz archive from
 the contents of web_static folder"""
-from fabric.api import local, lcd
+
+# if __name__ == '__main__':
+from fabric.api import local
 from datetime import datetime
 import os
 
@@ -11,22 +13,30 @@ def do_pack():
     compress it into an archive named by the date and time of its creation.
     """
     try:
+        # Create the archive name
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         archive_name = f"web_static_{date}.tgz"
+
+        # Start the packing
         directory_name = "versions"
         print(f"Packing web_static to {directory_name}/{archive_name}")
 
+        # Create the destination folder if it doesn't exist
         if not os.path.exists(directory_name):
             os.makedirs(directory_name)
-        with lcd(directory_name):
-            local(f"tar -zcvf {archive_name} ../web_static/")
-            archive_size = os.path.getsize(f"{archive_name}")
 
+        # Create the archive of the web_static folder
+        local(f"tar -czvf {directory_name}/{archive_name} web_static")
+
+        # Display the size of the archive
+        archive_size = os.path.getsize(f"{directory_name}/{archive_name}")
         print("web_static packed: {}/{} -> {}".format(directory_name,
                                                       archive_name,
                                                       archive_size))
 
-        return f"{directory_name}/{archive_name}"
+        # Retrun the archive path
+        archive_path = f"{directory_name}/{archive_name}"
+        return archive_path
 
     except Exception:
         return None
